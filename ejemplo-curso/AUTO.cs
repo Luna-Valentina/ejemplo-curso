92ae61218bc0511ae9cf078ba09854497f1fb76d
Valentina Luna <130167468+Luna-Valentina@users.noreply.github.com>
2023-04-07 13:18:52 -0300

    Add files via upload
    ejemplo curso

---
+++ ejemplo-curso/AUTO.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_AUTO
+{
+    internal class AUTO
+    {
+
+        #region Atributos
+        private string Marca;
+        private string Modelo;
+        private int Anio;
+        private double Motor;
+        private int CantidadPuertas;
+        private double Combustible;
+        private double Capacidad;
+        private double Reserva;
+        #endregion
+
+        #region Properties
+        public string marca { get => Marca; }
+        public string modelo { get => Modelo; }
+        public int anio { get => Anio; }
+        public double motor { get => Motor; }
+        public int cantidadPuertas { get => CantidadPuertas; }
+        public double combustible { get => Combustible; set => Combustible = value; }
+        public double capacidad { get => Capacidad; }
+        public double reserva { get => Reserva; set => Reserva = value; }
+        #endregion
+
+        #region Métodos
+        public string conducir(float kilometros)
+        {
+            float combustibleNecesario = kilometros / 11; //11 KM POR LITRO RECORRIDO
+            if (combustibleNecesario <= Combustible + 5) 
+            {
+                Combustible -= combustibleNecesario;
+                return "Los kilómetros pueden ser recorridos. Usted aún dispone de " + Combustible.ToString("0.00") + " litros de combustible.";
+            }
+            else return "No es posible recorrer los kilómetros ingresados. No dispone del combustible necesario";
+        }
+
+        public string cargarCombustible(double litros)
+        {
+            double litrosDerramados = 49.00;
+
+           // litros CANTIDAD QUE CARGO
+           // combustible lo que tiene cargado
+            while (litros > 0 && Combustible <= 49.00)
+            {
+
+                litros--;
+                Combustible++;
+
+                
+                
+               
+
+            }
+            litrosDerramados = litros;
+            return "Combustible recargado. En la recarga se derramaron " + litrosDerramados + " litros. Ahora usted dispone de " + combustible.ToString("0.00") + " litros en su tanque y una reserva de " + reserva + " litros.";
+        }
+        #endregion
+
+        #region Constructora
+        public AUTO(string marca, string modelo, int anio, float motor, int cantidadPuertas, double combustible)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Anio = anio;
+            Motor = motor;
+            CantidadPuertas = cantidadPuertas;
+            Combustible = combustible;
+            Capacidad = 49;
+            Reserva = 5;
+        }
+        #endregion
+
+    }
+}
---
+++ ejemplo-curso/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_AUTO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Iniciando la consola.");
+            AUTO auto_uno = new AUTO("Chevrolet", "Onix", 2021, 1.3, 5, 35);
+            //Console.WriteLine("El auto número uno es un " + auto_uno.marca + " " + auto_uno.modelo + " del año " + auto_uno.anio + ".");
+            AUTO auto_dos = new AUTO("Volkswagen", "Vento", 2022, 1.4, 5, 40);
+            //Console.WriteLine("El auto número dos es un " + auto_dos.marca + " " + auto_dos.modelo + " del año " + auto_dos.anio + ".");
+            Console.WriteLine(auto_uno.marca + " ¿puedes hacer un viaje de 570 kilómetros ahora mismo?");
+            Console.WriteLine(auto_uno.conducir(570));
+            Console.WriteLine("¿Y qué tal un viaje de 120 kilómetros?");
+            Console.WriteLine(auto_uno.conducir(120));
+            Console.WriteLine("¿Qué tal si ahora realizamos un viaje de 150 kilómetros?");
+            Console.WriteLine(auto_uno.conducir(150));
+            Console.WriteLine("Ahora un último viaje de 20 kilómetros.");
+            Console.WriteLine(auto_uno.conducir(20));
+            Console.WriteLine("¡Surgió una emergencia! Preciso un viaje de 55 kilómetros.");
+            Console.WriteLine(auto_uno.conducir(55));
+            Console.WriteLine("Luego de todos los viajes, el tanque tiene " + auto_uno.combustible.ToString("0.00") + " litros.");
+            Console.WriteLine("Hora de cargar combustible.");
+            Console.WriteLine("Cargando, por favor espere...");
+            Console.WriteLine(auto_uno.cargarCombustible(10));
+            Console.WriteLine(auto_uno.cargarCombustible(20));
+            Console.WriteLine(auto_uno.cargarCombustible(50));
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
